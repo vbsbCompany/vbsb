463fb8eba0e095d415ee9851ee92ed798724d1b9
Sumit <sumit@kodnest.com>
2023-04-08 15:27:57 +0530

    our presence fixed

--- components/PresenceSection/index.jsx
+++ components/PresenceSection/index.jsx
@@ -6,8 +6,7 @@ function PresenceSection() {
       <h2 className="text-center text-3xl mb-6">Our Presence</h2>
       <div className="presences grid lg:grid-cols-3 xl:grid-cols-4 gap-16">
         <div className="presence-card">
-          <h3>MAHARASHTA(India)</h3>
-          <p>CA Vaibhav Bhatnagar</p>
+          <h3>Mumbai,Maharastra</h3>
           <address>
             Mumbai Office: A-103, Satellite Royale, Gen A.K. Vaidya Marg,
             Goregaon East, Mumbai, Mumbai (MH) – 400063
@@ -17,8 +16,7 @@ function PresenceSection() {
           </p>
         </div>
         <div className="presence-card">
-          <h3>UTTAR PRADESH (INDIA)</h3>
-          <p>CA. Akhil Dixit</p>
+          <h3>Orai,Uttar Pradesh</h3>
           <address>
             Orai Office: Jalaun Road, Gen A.K. Near Hotel Shree Hari, New Patel
             Nagar, Orai (UP)- 285001
@@ -28,8 +26,7 @@ function PresenceSection() {
           </p>
         </div>
         <div className="presence-card">
-          <h3>Indore(India)</h3>
-          <p>CA Vaibhav Bhatnagar</p>
+          <h3>Indore,Madya Pradesh</h3>
           <address>
             405, Shreenathji Avenue, 158, RNT Marg,  Indore (MP) 452001
           </address>
@@ -38,8 +35,7 @@ function PresenceSection() {
           </p>
         </div>
         <div className="presence-card">
-          <h3>Hyderabad(India)</h3>
-          <p>CA Vaibhav Bhatnagar</p>
+          <h3>Hyderabad,Telangana</h3>
           <address>
             Shri Sai Nagar Colony, Beerumguda, Hyderabad, Telangana- 502032
           </address>
