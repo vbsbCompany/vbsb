a7da2e5c6baadf9f629e05642b8575ac31fd8650
Sumit <sumit@kodnest.com>
2023-07-04 13:39:46 +0530

    tds data connected with calender

---
+++ data/deadlines.js
@@ -0,0 +1,71 @@
+const tds = [
+  {
+    id: 1,
+    timestamp: "Sun May 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 2,
+    timestamp: "Wed Jun 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 3,
+    timestamp: "Fri Jul 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 4,
+    timestamp: "Mon Aug 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 5,
+    timestamp: "Thu Sep 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 6,
+    timestamp: "Sat Oct 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 7,
+    timestamp: "Tue Nov 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 8,
+    timestamp: "Thu Dec 07 2023 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 9,
+    timestamp: "Tue Jan 09 2024 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 10,
+    timestamp: "Wed Feb 07 2024 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 11,
+    timestamp: "Thu Mar 07 2024 00:00:00 GMT+0530 (India Standard Time)",
+    description: "Due Date for depositing TDS",
+  },
+  {
+    id: 12,
+    timestamp: "Sun Apr 07 2024 00:00:00 GMT+0530 (India Standard Time)",
+    description:
+      "Due Date for depositing TDS (for tax deducted by govt. office)",
+  },
+  {
+    id: 13,
+    timestamp: "Tue Apr 30 2024 00:00:00 GMT+0530 (India Standard Time)",
+    description:
+      "Due Date for depositing TDS (for tax deducted by govt. office)",
+  },
+];
+
+export { tds };
--- pages/deadlines/index.jsx
+++ pages/deadlines/index.jsx
@@ -1,33 +1,56 @@
 import Header from "@/components/Header";
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { tds } from "@/data/deadlines";
 
-function DeadlineList() {
+function DeadlineList({ date, description }) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  console.log("SELECTED DATE", date);
   return (
     <div className="deadline_list py-3 px-3 bg-green-100">
-      <h3 className="font-bold">7th June 2023</h3>
-      <p className="text-black/70">
-        ​​Due date for deposit of Tax deducted/collected for the month of May,
-        2023. However, all sum deducted/collected by an office of the government
-        shall be paid to the credit of the Central Government on the same day
-        where tax is paid without production of an Income-tax Challan
-      </p>
+      <h3 className="font-bold">
+        {`${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`}
+      </h3>
+      <p className="text-black/70">{description}</p>
     </div>
   );
 }
 function index() {
   const [value, onChange] = useState(new Date());
+  const [TDSDATA, setTdsData] = useState("");
 
   console.log("VALUE: ", value);
+
+  useEffect(() => {
+    console.log(value);
+    const filtered = tds.filter((d) => d.timestamp == value);
+    console.log("Filtered", filtered);
+    setTdsData(filtered.length > 0 ? filtered[0] : "");
+  }, [value]);
+
+  console.log("TDS DATA", TDSDATA);
   return (
     <div className="bg-gray-200">
       <Head>
         <title>Gallery &#8211; VBSB &#038; Associates</title>
       </Head>
       <Header index={4} />
-      <div className="w-full h-screen px-16 py-16 grid grid-cols-1 md:grid-cols-2 gap-32">
+      <div className="w-full h-screen px-6 md:px-16 py-16 grid grid-cols-1 md:grid-cols-2 gap-32">
         <div className="w-full flex flex-col items-start gap-12">
           <div className="deadline__controls__header">
             <select
@@ -43,13 +66,15 @@ function index() {
           </div>
           <Calendar onChange={onChange} value={value} className={"w-full"} />
         </div>
-        <div className="w-full bg-white px-6 py-6 shadow-md rounded-md max-h-[344px] overflow-y-auto flex flex-col items-start gap-3">
-          <DeadlineList />
-          <DeadlineList />
-          <DeadlineList />
-          <DeadlineList />
-          <DeadlineList />
-        </div>
+        {TDSDATA ? (
+          <div className="w-full px-6 py-6 rounded-md max-h-[344px] overflow-y-auto flex flex-col items-start gap-3">
+            <DeadlineList date={value} description={TDSDATA?.description} />
+          </div>
+        ) : (
+          <div className="w-full  px-6 py-6 rounded-md max-h-[344px] overflow-y-auto flex flex-col items-start gap-3">
+            <p>No Data Avialable</p>
+          </div>
+        )}
       </div>
     </div>
   );
