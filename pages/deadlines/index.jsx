cd68c0fd761007c32ca88aea4ceda6cda1a3af18
Sumit <sumit@kodnest.com>
2023-07-07 02:36:59 +0530

    deadlines fixed

--- pages/deadlines/index.jsx
+++ pages/deadlines/index.jsx
@@ -5,45 +5,48 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { income_tax, roc, tds } from "@/data/deadlines";
 import Footer from "@/components/Footer";
-
-function DeadlineList({ date, description }) {
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  console.log("SELECTED DATE", date);
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+function DeadlineList({ entry, description }) {
+  console.log("SELECTED DATE", entry);
   return (
     <div className="deadline_list py-3 px-3 bg-green-100 w-full">
       <h3 className="font-bold">
-        {`${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`}
+        {`${new Date(entry?.timestamp).getDate()} ${
+          months[new Date(entry?.timestamp).getMonth()]
+        } ${new Date(entry?.timestamp).getFullYear()}`}
       </h3>
       <p className="text-black/70">{description}</p>
     </div>
   );
 }
 function index() {
-  const [value, onChange] = useState(new Date());
+  const [value, setValue] = useState(new Date());
   const [option, setOption] = useState("tds");
   const [TDSDATA, setTdsData] = useState("");
 
-  console.log("VALUE: ", value);
+  console.log("VALUE: ", months[value.getMonth()]);
 
   useEffect(() => {
     console.log(value);
     var filtered = [];
 
     if (option == "tds") {
-      filtered = tds.filter((d) => d.timestamp == value);
+      filtered = tds.filter((d) =>
+        d.timestamp.includes(months[value.getMonth()])
+      );
     } else if (option == "roc") {
       filtered = roc.filter((d) => d.timestamp == value);
     } else if (option == "income_tax") {
@@ -61,7 +64,7 @@ function index() {
         <title>Gallery &#8211; VBSB &#038; Associates</title>
       </Head>
       <Header index={6} />
-      <div className="w-full  px-6 md:px-16 py-16 grid grid-cols-1  gap-32 bg-white">
+      <div className="w-full  px-6 md:px-16 py-16 grid grid-cols-1 md:grid-cols-2 gap-32 bg-white">
         <div className="w-full flex flex-col items-start gap-12">
           <div>
             <h1 className="text-3xl">Tax Calendar</h1>
@@ -73,7 +76,6 @@ function index() {
               className="px-3 py-3 border border-black"
               onChange={(e) => {
                 setOption(e.target.value);
-                onChange(new Date());
               }}
             >
               <option value="tds">TDS</option>
@@ -82,7 +84,21 @@ function index() {
               <option value="income_tax">Income Tax</option>
             </select>
           </div>
-          <Calendar onChange={onChange} value={value} className={"w-full"} />
+          <Calendar
+            onChange={(val, event) => {
+              console.log("Value", value);
+              setValue(val);
+            }}
+            value={value}
+            className={"w-full"}
+            onClickMonth={() => {
+              console.log("New Month Changed");
+            }}
+            onActiveStartDateChange={(activeStartDate) => {
+              console.log(activeStartDate);
+              setValue(activeStartDate.activeStartDate);
+            }}
+          />
         </div>
 
         {TDSDATA.length > 0 ? (
@@ -95,7 +111,7 @@ function index() {
                 return (
                   <DeadlineList
                     key={i}
-                    date={value}
+                    entry={d}
                     description={d?.description}
                   />
                 );
